9a3b799056f8383329bf88d955ef629e7ed36629
Ryan Campbell <ryan.campbell4@gmail.com>
2025-01-05 22:54:41 -0600

    Add form page stub

--- server.py
+++ server.py
@@ -14,5 +14,13 @@ def about_page():
 def contact_page():
    return render_template('contact.html')
 
+@app.route('/form')
+def form_page():
+   return render_template('form-page.html')
+
+@app.post('/create-account')
+def create_account():
+   return 'Successfully created account!'
+
 if __name__ == '__main__':
    app.run(port=8000, debug=True)
