23ff1216d687e7831f95758af4ad40a19fae84c1
Ryan Campbell <ryan.campbell4@gmail.com>
2025-01-05 21:02:21 -0600

    Update template and render parameter

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-   return render_template('index.html')
+   return render_template('index.html', first_name='Ryan')
 
 if __name__ == '__main__':
    app.run(port=8000, debug=True)
